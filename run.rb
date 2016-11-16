6aa10b5f72d1e28986103f9c86ff54917fabc543
James Harris <computersherpa@gmail.com>
2016-11-15 18:57:29 -0600

    Next step

--- run.rb
+++ run.rb
@@ -16,3 +16,8 @@ fetcher = FetchHistory.new(github)
 
 results = fetcher.get_private_repos
 pp fetcher.populate_repo_commits(results)
+
+# Next step:
+# Script to create new commits
+# GIT_COMMITTER_DATE="Sat Jan 9 22:13:13 2016 -0700" GIT_AUTHOR_DATE="Sat Jan 9 22:13:13
+# 2016 -0700" git commit -m "jan 9 2016"
